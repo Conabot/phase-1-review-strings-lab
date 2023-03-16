917b92cc66f5822595eeded2ac8046c2bd1f477d
Minh Tran <tdaimike@gmail.com>
2023-03-16 11:46:37 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = "Panda";
+
+const welcomeMessage = `Welcome to Flatbook, currentUser + ${currentUser}!`; 
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, + ${currentUser.slice(0, 1)}!`;
+
+
